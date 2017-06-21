2e5086a510e0f5202e1f99e88430720e92a739e0
bit-zyl <ylzhai@bit.edu.cn>
2017-06-21 12:27:56 -0400

    Add files via upload

---
+++ core/server/src/main/java/alluxio/master/AlluxioMasterRestServiceHandler.java
@@ -0,0 +1,402 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.master;
+
+import alluxio.Configuration;
+import alluxio.Constants;
+import alluxio.MasterStorageTierAssoc;
+import alluxio.PropertyKey;
+import alluxio.RestUtils;
+import alluxio.RuntimeConstants;
+import alluxio.master.block.BlockMaster;
+import alluxio.master.file.FileSystemMaster;
+import alluxio.metrics.MetricsSystem;
+import alluxio.underfs.UnderFileSystem;
+import alluxio.web.MasterUIWebServer;
+import alluxio.wire.WorkerInfo;
+
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Gauge;
+import com.qmino.miredot.annotations.ReturnType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import javax.annotation.concurrent.NotThreadSafe;
+import javax.servlet.ServletContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * This class is a REST handler for requesting general master information.
+ */
+@NotThreadSafe
+@Path(AlluxioMasterRestServiceHandler.SERVICE_PREFIX)
+@Produces(MediaType.APPLICATION_JSON)
+public final class AlluxioMasterRestServiceHandler {
+  private static final Logger LOG = LoggerFactory.getLogger(Constants.LOGGER_TYPE);
+  public static final String SERVICE_PREFIX = "master";
+  public static final String GET_RPC_ADDRESS = "rpc_address";
+  public static final String GET_CONFIGURATION = "configuration";
+  public static final String GET_CAPACITY_BYTES = "capacity_bytes";
+  public static final String GET_USED_BYTES = "used_bytes";
+  public static final String GET_FREE_BYTES = "free_bytes";
+  public static final String GET_CAPACITY_BYTES_ON_TIERS = "capacity_bytes_on_tiers";
+  public static final String GET_USED_BYTES_ON_TIERS = "used_bytes_on_tiers";
+  public static final String GET_UFS_CAPACITY_BYTES = "ufs_capacity_bytes";
+  public static final String GET_UFS_USED_BYTES = "ufs_used_bytes";
+  public static final String GET_UFS_FREE_BYTES = "ufs_free_bytes";
+  public static final String GET_METRICS = "metrics";
+  public static final String GET_START_TIME_MS = "start_time_ms";
+  public static final String GET_UPTIME_MS = "uptime_ms";
+  public static final String GET_VERSION = "version";
+  public static final String GET_WORKER_COUNT = "worker_count";
+  public static final String GET_WORKER_INFO_LIST = "worker_info_list";
+
+  private final AlluxioMaster mMaster;
+  private final BlockMaster mBlockMaster;
+  private final String mUfsRoot = Configuration.get(PropertyKey.UNDERFS_ADDRESS);
+  private final UnderFileSystem mUfs = UnderFileSystem.get(mUfsRoot);
+
+  /**
+   * Constructs a new {@link AlluxioMasterRestServiceHandler}.
+   *
+   * @param context context for the servlet
+   */
+  public AlluxioMasterRestServiceHandler(@Context ServletContext context) {
+    // Poor man's dependency injection through the Jersey application scope.
+    mMaster =
+        (AlluxioMaster) context.getAttribute(MasterUIWebServer.ALLUXIO_MASTER_SERVLET_RESOURCE_KEY);
+    mBlockMaster = mMaster.getBlockMaster();
+  }
+
+  /**
+   * @summary get the configuration map, the keys are ordered alphabetically.
+   * @return the response object
+   */
+  @GET
+  @Path(GET_CONFIGURATION)
+  @ReturnType("java.util.SortedMap<java.lang.String, java.lang.String>")
+  public Response getConfiguration() {
+    return RestUtils.call(new RestUtils.RestCallable<Map<String, String>>() {
+      @Override
+      public Map<String, String> call() throws Exception {
+        Set<Map.Entry<String, String>> properties = Configuration.toMap().entrySet();
+        SortedMap<String, String> configuration = new TreeMap<>();
+        for (Map.Entry<String, String> entry : properties) {
+          String key = entry.getKey();
+          if (PropertyKey.isValid(key)) {
+            configuration.put(key, entry.getValue());
+          }
+        }
+        return configuration;
+      }
+    });
+  }
+
+  /**
+   * @summary get the master rpc address
+   * @return the response object
+   */
+  @GET
+  @Path(GET_RPC_ADDRESS)
+  @ReturnType("java.lang.String")
+  public Response getRpcAddress() {
+    return RestUtils.call(new RestUtils.RestCallable<String>() {
+      @Override
+      public String call() throws Exception {
+        return mMaster.getMasterAddress().toString();
+      }
+    });
+  }
+
+  /**
+   * @summary get the master metrics, the keys are ordered alphabetically.
+   * @return the response object
+   */
+  @GET
+  @Path(GET_METRICS)
+  @ReturnType("java.util.SortedMap<java.lang.String, java.lang.Long>")
+  public Response getMetrics() {
+    return RestUtils.call(new RestUtils.RestCallable<Map<String, Long>>() {
+      @Override
+      public Map<String, Long> call() throws Exception {
+        // Get all counters.
+        Map<String, Counter> counters = MetricsSystem.METRIC_REGISTRY.getCounters();
+
+        // Only the gauge for pinned files is retrieved here, other gauges are statistics of
+        // free/used
+        // spaces, those statistics can be gotten via other REST apis.
+        String filesPinnedProperty =
+            MetricsSystem.getMasterMetricName(FileSystemMaster.Metrics.FILES_PINNED);
+        @SuppressWarnings("unchecked") Gauge<Integer> filesPinned =
+            (Gauge<Integer>) MetricsSystem.METRIC_REGISTRY.getGauges().get(filesPinnedProperty);
+
+        // Get values of the counters and gauges and put them into a metrics map.
+        SortedMap<String, Long> metrics = new TreeMap<>();
+        for (Map.Entry<String, Counter> counter : counters.entrySet()) {
+          metrics.put(counter.getKey(), counter.getValue().getCount());
+        }
+        metrics.put(filesPinnedProperty, filesPinned.getValue().longValue());
+
+        return metrics;
+      }
+    });
+  }
+
+  /**
+   * @summary get the start time of the master
+   * @return the response object
+   */
+  @GET
+  @Path(GET_START_TIME_MS)
+  @ReturnType("java.lang.Long")
+  public Response getStartTimeMs() {
+    return RestUtils.call(new RestUtils.RestCallable<Long>() {
+      @Override
+      public Long call() throws Exception {
+        return mMaster.getStartTimeMs();
+      }
+    });
+  }
+
+  /**
+   * @summary get the uptime of the master
+   * @return the response object
+   */
+  @GET
+  @Path(GET_UPTIME_MS)
+  @ReturnType("java.lang.Long")
+  public Response getUptimeMs() {
+    return RestUtils.call(new RestUtils.RestCallable<Long>() {
+      @Override
+      public Long call() throws Exception {
+        return mMaster.getUptimeMs();
+      }
+    });
+  }
+
+  /**
+   * @summary get the version of the master
+   * @return the response object
+   */
+  @GET
+  @Path(GET_VERSION)
+  @ReturnType("java.lang.String")
+  public Response getVersion() {
+    return RestUtils.call(new RestUtils.RestCallable<String>() {
+      @Override
+      public String call() throws Exception {
+        return RuntimeConstants.VERSION;
+      }
+    });
+  }
+
+  /**
+   * @summary get the total capacity of all workers in bytes
+   * @return the response object
+   */
+  @GET
+  @Path(GET_CAPACITY_BYTES)
+  @ReturnType("java.lang.Long")
+  public Response getCapacityBytes() {
+    return RestUtils.call(new RestUtils.RestCallable<Long>() {
+      @Override
+      public Long call() throws Exception {
+        return mBlockMaster.getCapacityBytes();
+      }
+    });
+  }
+
+  /**
+   * @summary get the used capacity
+   * @return the response object
+   */
+  @GET
+  @Path(GET_USED_BYTES)
+  @ReturnType("java.lang.Long")
+  public Response getUsedBytes() {
+    return RestUtils.call(new RestUtils.RestCallable<Long>() {
+      @Override
+      public Long call() throws Exception {
+        return mBlockMaster.getUsedBytes();
+      }
+    });
+  }
+
+  /**
+   * @summary get the free capacity
+   * @return the response object
+   */
+  @GET
+  @Path(GET_FREE_BYTES)
+  @ReturnType("java.lang.Long")
+  public Response getFreeBytes() {
+    return RestUtils.call(new RestUtils.RestCallable<Long>() {
+      @Override
+      public Long call() throws Exception {
+        return mBlockMaster.getCapacityBytes() - mBlockMaster.getUsedBytes();
+      }
+    });
+  }
+
+  /**
+   * @summary get the total ufs capacity in bytes, a negative value means the capacity is unknown.
+   * @return the response object
+   */
+  @GET
+  @Path(GET_UFS_CAPACITY_BYTES)
+  @ReturnType("java.lang.Long")
+  public Response getUfsCapacityBytes() {
+    return RestUtils.call(new RestUtils.RestCallable<Long>() {
+      @Override
+      public Long call() throws Exception {
+        return mUfs.getSpace(mUfsRoot, UnderFileSystem.SpaceType.SPACE_TOTAL);
+      }
+    });
+  }
+
+  /**
+   * @summary get the used disk capacity, a negative value means the capacity is unknown.
+   * @return the response object
+   */
+  @GET
+  @Path(GET_UFS_USED_BYTES)
+  @ReturnType("java.lang.Long")
+  public Response getUfsUsedBytes() {
+    return RestUtils.call(new RestUtils.RestCallable<Long>() {
+      @Override
+      public Long call() throws Exception {
+        return mUfs.getSpace(mUfsRoot, UnderFileSystem.SpaceType.SPACE_USED);
+      }
+    });
+  }
+
+  /**
+   * @summary get the free ufs capacity in bytes, a negative value means the capacity is unknown.
+   * @return the response object
+   */
+  @GET
+  @Path(GET_UFS_FREE_BYTES)
+  @ReturnType("java.lang.Long")
+  public Response getUfsFreeBytes() {
+    return RestUtils.call(new RestUtils.RestCallable<Long>() {
+      @Override
+      public Long call() throws Exception {
+        return mUfs.getSpace(mUfsRoot, UnderFileSystem.SpaceType.SPACE_FREE);
+      }
+    });
+  }
+
+  private Comparator<String> getTierAliasComparator() {
+    return new Comparator<String>() {
+      private MasterStorageTierAssoc mTierAssoc = new MasterStorageTierAssoc();
+
+      @Override
+      public int compare(String tier1, String tier2) {
+        int ordinal1 = mTierAssoc.getOrdinal(tier1);
+        int ordinal2 = mTierAssoc.getOrdinal(tier2);
+        if (ordinal1 < ordinal2) {
+          return -1;
+        }
+        if (ordinal1 == ordinal2) {
+          return 0;
+        }
+        return 1;
+      }
+    };
+  }
+
+  /**
+   * @summary get the mapping from tier alias to total capacity of the tier in bytes, keys are in
+   *    the order from tier alias with smaller ordinal to those with larger ones.
+   * @return the response object
+   */
+  @GET
+  @Path(GET_CAPACITY_BYTES_ON_TIERS)
+  @ReturnType("java.util.SortedMap<java.lang.String, java.lang.Long>")
+  public Response getCapacityBytesOnTiers() {
+    return RestUtils.call(new RestUtils.RestCallable<Map<String, Long>>() {
+      @Override
+      public Map<String, Long> call() throws Exception {
+        SortedMap<String, Long> capacityBytesOnTiers = new TreeMap<>(getTierAliasComparator());
+        for (Map.Entry<String, Long> tierBytes : mBlockMaster.getTotalBytesOnTiers().entrySet()) {
+          capacityBytesOnTiers.put(tierBytes.getKey(), tierBytes.getValue());
+        }
+        return capacityBytesOnTiers;
+      }
+    });
+  }
+
+  /**
+   * @summary get the mapping from tier alias to the used bytes of the tier, keys are in the order
+   *    from tier alias with smaller ordinal to those with larger ones.
+   * @return the response object
+   */
+  @GET
+  @Path(GET_USED_BYTES_ON_TIERS)
+  @ReturnType("java.util.SortedMap<java.lang.String, java.lang.Long>")
+  public Response getUsedBytesOnTiers() {
+    return RestUtils.call(new RestUtils.RestCallable<Map<String, Long>>() {
+      @Override
+      public Map<String, Long> call() throws Exception {
+        SortedMap<String, Long> usedBytesOnTiers = new TreeMap<>(getTierAliasComparator());
+        for (Map.Entry<String, Long> tierBytes : mBlockMaster.getUsedBytesOnTiers().entrySet()) {
+          usedBytesOnTiers.put(tierBytes.getKey(), tierBytes.getValue());
+        }
+        return usedBytesOnTiers;
+      }
+    });
+  }
+
+  /**
+   * @summary get the count of workers
+   * @return the response object
+   */
+  @GET
+  @Path(GET_WORKER_COUNT)
+  @ReturnType("java.lang.Integer")
+  public Response getWorkerCount() {
+    return RestUtils.call(new RestUtils.RestCallable<Integer>() {
+      @Override
+      public Integer call() throws Exception {
+        return mBlockMaster.getWorkerCount();
+      }
+    });
+  }
+
+  /**
+   * @summary get the list of worker descriptors
+   * @return the response object
+   */
+  @GET
+  @Path(GET_WORKER_INFO_LIST)
+  @ReturnType("java.util.List<alluxio.wire.WorkerInfo>")
+  public Response getWorkerInfoList() {
+    return RestUtils.call(new RestUtils.RestCallable<List<WorkerInfo>>() {
+      @Override
+      public List<WorkerInfo> call() throws Exception {
+        return mBlockMaster.getWorkerInfoList();
+      }
+    });
+  }
+}
---
+++ core/server/src/main/java/alluxio/master/file/meta/Inode.java
@@ -0,0 +1,345 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.master.file.meta;
+
+import alluxio.Constants;
+import alluxio.master.journal.JournalEntryRepresentable;
+import alluxio.security.authorization.Permission;
+import alluxio.wire.FileInfo;
+
+import com.google.common.base.Objects;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+/**
+ * {@link Inode} is an abstract class, with information shared by all types of Inodes. The inode
+ * must be locked ({@link #lockRead()} or {@link #lockWrite()}) before methods are called.
+ *
+ * @param <T> the concrete subclass of this object
+ */
+@NotThreadSafe
+public abstract class Inode<T> implements JournalEntryRepresentable {
+  protected long mCreationTimeMs;
+  private boolean mDeleted;
+  protected boolean mDirectory;
+  protected long mId;
+  private long mLastModificationTimeMs;
+  private String mName;
+  private long mParentId;
+  private PersistenceState mPersistenceState;
+  private boolean mPinned;
+
+  private String mOwner;
+  private String mGroup;
+  private short mMode;
+
+  private final ReentrantReadWriteLock mLock;
+
+  protected Inode(long id, boolean isDirectory) {
+    mCreationTimeMs = System.currentTimeMillis();
+    mDeleted = false;
+    mDirectory = isDirectory;
+    mGroup = "";
+    mId = id;
+    mLastModificationTimeMs = mCreationTimeMs;
+    mName = null;
+    mParentId = InodeTree.NO_PARENT;
+    mMode = Constants.INVALID_MODE;
+    mPersistenceState = PersistenceState.NOT_PERSISTED;
+    mPinned = false;
+    mOwner = "";
+    mLock = new ReentrantReadWriteLock();
+  }
+
+  /**
+   * @return the create time, in milliseconds
+   */
+  public long getCreationTimeMs() {
+    return mCreationTimeMs;
+  }
+
+  /**
+   * @return the group of the inode
+   */
+  public String getGroup() {
+    return mGroup;
+  }
+
+  /**
+   * @return the id of the inode
+   */
+  public long getId() {
+    return mId;
+  }
+
+  /**
+   * @return the last modification time, in milliseconds
+   */
+  public long getLastModificationTimeMs() {
+    return mLastModificationTimeMs;
+  }
+
+  /**
+   * @return the name of the inode
+   */
+  public String getName() {
+    return mName;
+  }
+
+  /**
+   * @return the mode of the inode
+   */
+  public short getMode() {
+    return mMode;
+  }
+
+  /**
+   * @return the {@link PersistenceState} of the inode
+   */
+  public PersistenceState getPersistenceState() {
+    return mPersistenceState;
+  }
+
+  /**
+   * @return the id of the parent folder
+   */
+  public long getParentId() {
+    return mParentId;
+  }
+
+  /**
+   * @return the owner of the inode
+   */
+  public String getOwner() {
+    return mOwner;
+  }
+
+  /**
+   * @return true if the inode is deleted, false otherwise
+   */
+  public boolean isDeleted() {
+    return mDeleted;
+  }
+
+  /**
+   * @return true if the inode is a directory, false otherwise
+   */
+  public boolean isDirectory() {
+    return mDirectory;
+  }
+
+  /**
+   * @return true if the inode is a file, false otherwise
+   */
+  public boolean isFile() {
+    return !mDirectory;
+  }
+
+  /**
+   * @return true if the inode is pinned, false otherwise
+   */
+  public boolean isPinned() {
+    return mPinned;
+  }
+
+  /**
+   * @return true if the file has persisted, false otherwise
+   */
+  public boolean isPersisted() {
+    return mPersistenceState == PersistenceState.PERSISTED;
+  }
+
+  /**
+   * @param creationTimeMs the creation time to use (in milliseconds)
+   * @return the updated object
+   */
+  public T setCreationTimeMs(long creationTimeMs) {
+    mCreationTimeMs = creationTimeMs;
+    return getThis();
+  }
+
+  /**
+   * @param deleted the deleted flag to use
+   * @return the updated object
+   */
+  public T setDeleted(boolean deleted) {
+    mDeleted = deleted;
+    return getThis();
+  }
+
+  /**
+   * @param group the group of the inode
+   * @return the updated object
+   */
+  public T setGroup(String group) {
+    mGroup = group;
+    return getThis();
+  }
+
+  /**
+   * @param lastModificationTimeMs the last modification time to use
+   * @return the updated object
+   */
+  public T setLastModificationTimeMs(long lastModificationTimeMs) {
+    mLastModificationTimeMs = lastModificationTimeMs;
+    return getThis();
+  }
+
+  /**
+   * @param name the name to use
+   * @return the updated object
+   */
+  public T setName(String name) {
+    mName = name;
+    return getThis();
+  }
+
+  /**
+   * @param parentId the parent id to use
+   * @return the updated object
+   */
+  public T setParentId(long parentId) {
+    mParentId = parentId;
+    return getThis();
+  }
+
+  /**
+   * @param persistenceState the {@link PersistenceState} to use
+   * @return the updated object
+   */
+  public T setPersistenceState(PersistenceState persistenceState) {
+    mPersistenceState = persistenceState;
+    return getThis();
+  }
+
+  /**
+   * @param permission the {@link Permission} to use
+   * @return the updated object
+   */
+  public T setPermission(Permission permission) {
+    if (permission != null) {
+      mOwner = permission.getOwner();
+      mGroup = permission.getGroup();
+      mMode = permission.getMode().toShort();
+    }
+    return getThis();
+  }
+
+  /**
+   * @param mode the mode of the inode
+   * @return the updated object
+   */
+  public T setPermission(short mode) {
+    mMode = mode;
+    return getThis();
+  }
+
+  /**
+   * @param pinned the pinned flag value to use
+   * @return the updated object
+   */
+  public T setPinned(boolean pinned) {
+    mPinned = pinned;
+    return getThis();
+  }
+
+  /**
+   * @param owner the owner name of the inode
+   * @return the updated object
+   */
+  public T setOwner(String owner) {
+    mOwner = owner;
+    return getThis();
+  }
+
+  /**
+   * Generates a {@link FileInfo} of the file or folder.
+   *
+   * @param path the path of the file
+   * @return generated {@link FileInfo}
+   */
+  public abstract FileInfo generateClientFileInfo(String path);
+
+  /**
+   * @return {@code this} so that the abstract class can use the fluent builder pattern
+   */
+  protected abstract T getThis();
+
+  /**
+   * Acquires the read lock for this inode.
+   */
+  public void lockRead() {
+    mLock.readLock().lock();
+  }
+
+  /**
+   * Releases the read lock for this inode.
+   */
+  public void unlockRead() {
+    mLock.readLock().unlock();
+  }
+
+  /**
+   * Acquires the write lock for this inode.
+   */
+  public void lockWrite() {
+    mLock.writeLock().lock();
+  }
+
+  /**
+   * Releases the write lock for this inode.
+   */
+  public void unlockWrite() {
+    mLock.writeLock().unlock();
+  }
+
+  /**
+   * @return returns true if the current thread holds a write lock on this inode, false otherwise
+   */
+  public boolean isWriteLocked() {
+    return mLock.isWriteLockedByCurrentThread();
+  }
+
+  /**
+   * @return returns true if the current thread holds a read lock on this inode, false otherwise
+   */
+  public boolean isReadLocked() {
+    return mLock.getReadHoldCount() > 0;
+  }
+
+  @Override
+  public int hashCode() {
+    return ((Long) mId).hashCode();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Inode<?>)) {
+      return false;
+    }
+    Inode<?> that = (Inode<?>) o;
+    return mId == that.mId;
+  }
+
+  protected Objects.ToStringHelper toStringHelper() {
+    return Objects.toStringHelper(this).add("id", mId).add("name", mName).add("parentId", mParentId)
+        .add("creationTimeMs", mCreationTimeMs).add("pinned", mPinned).add("deleted", mDeleted)
+        .add("directory", mDirectory).add("persistenceState", mPersistenceState)
+        .add("lastModificationTimeMs", mLastModificationTimeMs).add("owner", mOwner)
+        .add("group", mGroup).add("permission", mMode);
+  }
+}
