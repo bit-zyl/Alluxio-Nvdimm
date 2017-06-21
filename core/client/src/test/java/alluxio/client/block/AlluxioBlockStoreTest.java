869a237f63d151bebcc80e12b57cf755609a50f0
bit-zyl <ylzhai@bit.edu.cn>
2017-06-21 12:08:58 -0400

    Add files via upload

---
+++ core/client/src/test/java/alluxio/client/block/AlluxioBlockStoreTest.java
@@ -0,0 +1,113 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.client.block;
+
+import alluxio.resource.DummyCloseableResource;
+import alluxio.wire.BlockInfo;
+import alluxio.wire.BlockLocation;
+import alluxio.wire.LockBlockResult;
+import alluxio.wire.WorkerNetAddress;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.io.File;
+import java.util.Arrays;
+
+/**
+ * Tests for {@link AlluxioBlockStore}.
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({BlockStoreContext.class})
+public final class AlluxioBlockStoreTest {
+  private static final long BLOCK_ID = 3L;
+  private static final long LOCK_ID = 44L;
+  private static final String WORKER_HOSTNAME_LOCAL = "localhost";
+  private static final String WORKER_HOSTNAME_REMOTE = "remote";
+  private static final WorkerNetAddress WORKER_NET_ADDRESS_LOCAL = new WorkerNetAddress()
+      .setHost(WORKER_HOSTNAME_LOCAL);
+  private static final WorkerNetAddress WORKER_NET_ADDRESS_REMOTE = new WorkerNetAddress()
+      .setHost(WORKER_HOSTNAME_REMOTE);
+  private static final BlockLocation BLOCK_LOCATION_LOCAL = new BlockLocation()
+      .setWorkerAddress(WORKER_NET_ADDRESS_LOCAL);
+  private static final BlockLocation BLOCK_LOCATION_REMOTE = new BlockLocation()
+      .setWorkerAddress(WORKER_NET_ADDRESS_REMOTE);
+
+  /**
+   * The rule for a temporary folder.
+   */
+  @Rule
+  public TemporaryFolder mTestFolder = new TemporaryFolder();
+
+  private BlockMasterClient mMasterClient;
+  private BlockWorkerClient mBlockWorkerClient;
+  private AlluxioBlockStore mBlockStore;
+
+  @Before
+  public void before() throws Exception {
+    mBlockWorkerClient = PowerMockito.mock(BlockWorkerClient.class);
+    mMasterClient = PowerMockito.mock(BlockMasterClient.class);
+
+    BlockStoreContext blockStoreContext = PowerMockito.mock(BlockStoreContext.class);
+    // Mock block store context to return our mock clients
+    Mockito.when(blockStoreContext.acquireWorkerClient(Mockito.any(WorkerNetAddress.class)))
+        .thenReturn(mBlockWorkerClient);
+    Mockito.when(blockStoreContext.acquireMasterClientResource()).thenReturn(
+        new DummyCloseableResource<>(mMasterClient));
+
+    mBlockStore = new AlluxioBlockStore(blockStoreContext, WORKER_HOSTNAME_LOCAL);
+  }
+
+  /**
+   * Tests {@link AlluxioBlockStore#getInStream(long)} when a local block exists, making sure that
+   * the local block is preferred.
+   */
+  @Test
+  public void getInStreamLocal() throws Exception {
+    Mockito.when(mMasterClient.getBlockInfo(BLOCK_ID)).thenReturn(new BlockInfo()
+        .setLocations(Arrays.asList(BLOCK_LOCATION_REMOTE, BLOCK_LOCATION_LOCAL)));
+
+    File mTestFile = mTestFolder.newFile("testFile");
+    // When a block lock for id BLOCK_ID is requested, a path to a temporary file is returned
+    Mockito.when(mBlockWorkerClient.lockBlock(BLOCK_ID)).thenReturn(
+        new LockBlockResult().setLockId(LOCK_ID).setBlockPath(mTestFile.getAbsolutePath()));
+
+    BufferedBlockInStream stream = mBlockStore.getInStream(BLOCK_ID);
+    Assert.assertEquals(LocalBlockInStream.class, stream.getClass());
+  }
+
+  /**
+   * Tests {@link AlluxioBlockStore#getInStream(long)} when no local block exists, making sure that
+   * the first {@link BlockLocation} in the {@link BlockInfo} list is chosen.
+   */
+  @Test
+  public void getInStreamRemote() throws Exception {
+    Mockito.when(mMasterClient.getBlockInfo(BLOCK_ID)).thenReturn(new BlockInfo()
+        .setLocations(Arrays.asList(BLOCK_LOCATION_REMOTE)));
+
+    File mTestFile = mTestFolder.newFile("testFile");
+    // When a block lock for id BLOCK_ID is requested, a path to a temporary file is returned
+    Mockito.when(mBlockWorkerClient.lockBlock(BLOCK_ID)).thenReturn(
+        new LockBlockResult().setLockId(LOCK_ID).setBlockPath(mTestFile.getAbsolutePath()));
+
+    BufferedBlockInStream stream = mBlockStore.getInStream(BLOCK_ID);
+    Assert.assertEquals(RemoteBlockInStream.class, stream.getClass());
+  }
+}
---
+++ core/client/src/test/java/alluxio/client/file/options/OutStreamOptionsTest.java
@@ -0,0 +1,101 @@
+/*
+ * The Alluxio Open Foundation licenses this work under the Apache License, version 2.0
+ * (the "License"). You may not use this work except in compliance with the License, which is
+ * available at www.apache.org/licenses/LICENSE-2.0
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied, as more fully set forth in the License.
+ *
+ * See the NOTICE file distributed with this work for information regarding copyright ownership.
+ */
+
+package alluxio.client.file.options;
+
+import alluxio.AuthenticatedUserRule;
+import alluxio.CommonTestUtils;
+import alluxio.Configuration;
+import alluxio.ConfigurationTestUtils;
+import alluxio.Constants;
+import alluxio.PropertyKey;
+import alluxio.client.AlluxioStorageType;
+import alluxio.client.UnderStorageType;
+import alluxio.client.WriteType;
+import alluxio.client.file.policy.FileWriteLocationPolicy;
+import alluxio.client.file.policy.LocalFirstPolicy;
+import alluxio.client.file.policy.RoundRobinPolicy;
+import alluxio.security.authorization.Permission;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.io.IOException;
+import java.util.Random;
+
+/**
+ * Tests for the {@link OutStreamOptions} class.
+ */
+@RunWith(PowerMockRunner.class)
+// Need to mock Permission to use CommonTestUtils#testEquals.
+@PrepareForTest(Permission.class)
+public class OutStreamOptionsTest {
+  @Rule
+  public AuthenticatedUserRule mRule = new AuthenticatedUserRule("test");
+
+  /**
+   * Tests that building an {@link OutStreamOptions} with the defaults works.
+   */
+  @Test
+  public void defaults() throws IOException {
+    AlluxioStorageType alluxioType = AlluxioStorageType.STORE;
+    UnderStorageType ufsType = UnderStorageType.SYNC_PERSIST;
+    Configuration.set(PropertyKey.USER_BLOCK_SIZE_BYTES_DEFAULT, "64MB");
+    Configuration.set(PropertyKey.USER_FILE_WRITE_TYPE_DEFAULT, WriteType.CACHE_THROUGH.toString());
+
+    OutStreamOptions options = OutStreamOptions.defaults();
+
+    Assert.assertEquals(64 * Constants.MB, options.getBlockSizeBytes());
+    Assert.assertEquals(alluxioType, options.getAlluxioStorageType());
+    Assert.assertEquals(Constants.NO_TTL, options.getTtl());
+    Assert.assertEquals(ufsType, options.getUnderStorageType());
+    Assert.assertTrue(options.getLocationPolicy() instanceof LocalFirstPolicy);
+    Assert.assertEquals(Permission.defaults().applyFileUMask().setOwnerFromLoginModule(),
+        options.getPermission());
+    ConfigurationTestUtils.resetConfiguration();
+  }
+
+  /**
+   * Tests getting and setting fields.
+   */
+  @Test
+  public void fields() {
+    Random random = new Random();
+    long blockSize = random.nextLong();
+    FileWriteLocationPolicy policy = new RoundRobinPolicy();
+    long ttl = random.nextLong();
+    WriteType writeType = WriteType.NONE;
+    Permission perm = Permission.defaults();
+
+    OutStreamOptions options = OutStreamOptions.defaults();
+    options.setBlockSizeBytes(blockSize);
+    options.setLocationPolicy(policy);
+    options.setTtl(ttl);
+    options.setWriteType(writeType);
+    options.setPermission(perm);
+
+    Assert.assertEquals(blockSize, options.getBlockSizeBytes());
+    Assert.assertEquals(policy, options.getLocationPolicy());
+    Assert.assertEquals(ttl, options.getTtl());
+    Assert.assertEquals(writeType.getAlluxioStorageType(), options.getAlluxioStorageType());
+    Assert.assertEquals(writeType.getUnderStorageType(), options.getUnderStorageType());
+    Assert.assertEquals(perm, options.getPermission());
+  }
+
+  @Test
+  public void equalsTest() throws Exception {
+    CommonTestUtils.testEquals(OutStreamOptions.class);
+  }
+}
